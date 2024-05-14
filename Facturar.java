5e541bf36b1ee4bbe66fab20b47de30269f48701
rodrigodelama <rodrigo.delama@icloud.com>
2024-05-14 22:07:02 +0200

    Paso 1 completado !!

--- Facturar.java
+++ Facturar.java
@@ -20,79 +20,93 @@ public class Facturar {
     */
 
     public static void main(String[] args) {
+
+        //Luis
+        // String addr = "10.202.20.125";
+        String addr = "127.0.0.1";
+
         // Inicializar credenciales para autenticación básica
-        HTTP.Init("usuario", "clave");
+        // HTTP.Init("usuario", "clave");
+        HTTP.Init("Pareja1", "rEf@ZO{TU");
 
         // a. Imprimir la lista de ids de clientes
         System.out.println("Ids de clientes");
-        JSONArray clientesIds = HTTP.Get_array("http://10.202.20.125/petrest/clientes");
+        JSONArray clientesIds = HTTP.Get_array("http://"+ addr + "/petrest/clientes");
         System.out.println(clientesIds);
 
         // b. Imprimir los clientes con id entre 1 y 5 individualmente
         System.out.println("\nClientes del 1 al 5");
         for (int i = 1; i <= 5; i++) {
-            JSONObject cliente = HTTP.Get_object("http://10.202.20.125/petrest/clientes/" + i);
+            JSONObject cliente = HTTP.Get_object("http://"+ addr + "/petrest/clientes/" + i);
             System.out.println("cliente: " + formatCliente(cliente));
         }
 
         // c. Imprimir la lista de los ids de los artículos
         System.out.println("\nIds de artículos");
-        JSONArray articulosIds = HTTP.Get_array("http://10.202.20.125/petrest/articulos");
+        JSONArray articulosIds = HTTP.Get_array("http://"+ addr + "/petrest/articulos");
         System.out.println(articulosIds);
 
         // d. Imprimir los artículos del 1 al 5 individualmente
         System.out.println("\nArtículos del 1 al 5");
         for (int i = 1; i <= 5; i++) {
-            JSONObject articulo = HTTP.Get_object("http://10.202.20.125/petrest/articulos/" + i);
+            JSONObject articulo = HTTP.Get_object("http://"+ addr + "/petrest/articulos/" + i);
             System.out.println("artículo: " + formatArticulo(articulo));
         }
 
         // e. Imprimir la lista de los ids de todos los pedidos
         System.out.println("\nIds de pedidos");
-        JSONArray pedidosIds = HTTP.Get_array("http://10.202.20.125/petrest/pedidos");
+        JSONArray pedidosIds = HTTP.Get_array("http://"+ addr + "/petrest/pedidos");
         System.out.println(pedidosIds);
 
         // f. Imprimir los pedidos con id entre 1 y 5 individualmente
         System.out.println("\nPedidos del 1 al 5");
         for (int i = 1; i <= 5; i++) {
-            JSONObject pedido = HTTP.Get_object("http://10.202.20.125/petrest/pedidos/" + i);
+            JSONObject pedido = HTTP.Get_object("http://"+ addr + "/petrest/pedidos/" + i);
             System.out.println("artículo: " + formatPedido(pedido));
         }
 
         // g. Imprimir los ítems del pedido 1
         System.out.println("\nItems del pedido 1");
-        JSONArray itemsPedido1 = HTTP.Get_array("http://10.202.20.125/petrest/pedidos/1/items");
+        JSONArray itemsPedido1 = HTTP.Get_array("http://"+ addr + "/petrest/pedidos/1/items");
         for (Object item : itemsPedido1) {
             System.out.println("item: " + formatItem((JSONObject) item));
         }
 
         // h. Restablecer las facturas
         System.out.println("\nResetear facturas");
-        HTTP.Delete("http://10.202.20.125/petrest/facturas");
+        HTTP.Delete("http://"+ addr + "/petrest/facturas");
         System.out.println("Hecho");
 
         // i. Crear una nueva factura con id_pedido=1, importe=1235.22
-        System.out.println("\nCrear factura");
         JSONObject facturaData = new JSONObject();
         facturaData.put("id_pedido", 1);
         facturaData.put("importe", 1235.22);
-        HTTP.Post("http://10.202.20.125/petrest/facturas", facturaData.toString());
+            //curl -X POST http://127.0.0.1/petrest/facturas -u "Pareja1:rEf@ZO{TU" -H "Content-Type:application/json" -d "{\"id_pedido\":1,\"importe\":1235.22}"
+        HTTP.Post("http://" + addr + "/petrest/facturas", facturaData.toString());
 
         // j. Modificar la factura con descuento=123.52, base=1111.70, iva=233.46, total=1345.16
-        int nuevaFacturaId = 68; // Cambia por el id real de la nueva factura
+        // GET request to retrieve the newly created factura
+        JSONArray idNuevaFactura = HTTP.Get_array("http://" + addr + "/petrest/facturas");
+        // int nuevaFacturaId = (Integer) idNuevaFactura.get(0); // It MUST be a long, since thats what we recieve
+        // Exception in thread "main" java.lang.ClassCastException: class java.lang.Long cannot be cast to class java.lang.Integer (java.lang.Long and java.lang.Integer are in module java.base of loader 'bootstrap')
+        // at Facturar.main(Facturar.java:94)
+
+        // Get the first element of the JSONArray
+        Long nuevaFacturaIdLong = (Long) idNuevaFactura.get(0);
+        // Convert the Long value to an int
+        int nuevaFacturaId = nuevaFacturaIdLong.intValue();
         JSONObject facturaModificacion = new JSONObject();
         facturaModificacion.put("descuento", 123.52);
         facturaModificacion.put("base", 1111.70);
         facturaModificacion.put("iva", 233.46);
         facturaModificacion.put("total", 1345.16);
-        HTTP.Put("http://10.202.20.125/petrest/facturas/" + nuevaFacturaId, facturaModificacion.toString());
-
+        HTTP.Put("http://"+ addr + "/petrest/facturas/" + nuevaFacturaId, facturaModificacion.toString());
         System.out.println("Completar factura");
         System.out.println("Hecho");
 
         // k. Imprimir la factura creada
-        System.out.println("\nFactura 68");
-        JSONObject factura = HTTP.Get_object("http://10.202.20.125/petrest/facturas/68");
+        System.out.println("\nFactura " + nuevaFacturaId);
+        JSONObject factura = HTTP.Get_object("http://"+ addr + "/petrest/facturas/" + nuevaFacturaId);
         System.out.println(formatFactura(factura));
     }
 
